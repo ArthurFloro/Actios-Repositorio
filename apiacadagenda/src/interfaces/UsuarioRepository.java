74e9767e0a1501685e8cbedb84b16bc9f0f5e6b9
arthur_floro <134660119+ArthurFloro@users.noreply.github.com>
2025-03-19 21:30:07 -0300

    Delete apiacadagenda directory
    deletei porque essa api estava quebrando

--- apiacadagenda/src/interfaces/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    Optional<Usuario> findByEmail(String email);
-}
